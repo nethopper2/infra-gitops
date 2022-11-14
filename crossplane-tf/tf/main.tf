bb62717cf15154f03ee5dfc71037b81151848b1d
chris munford <chris_munford@yahoo.com>
2022-11-14 09:14:15 -0500

    remove image

--- crossplane-tf/tf/main.tf
+++ crossplane-tf/tf/main.tf
@@ -23,17 +23,17 @@
         wait_timeout = 60
       }
 
-      resource "nutanix_image" "image" {
-        # name        = "ubuntu-20.04.2.0-desktop-amd64.iso"
-        name        = "ubuntu-20.04.5-desktop-amd64.iso"
-        # description = "Ubuntu 20.04 LTS"
-        source_uri = "https://releases.ubuntu.com/focal/ubuntu-20.04.5-desktop-amd64.iso"
-        # id = "f7542282-97a2-4bf5-ade9-c4a34e3ff7d1"
-        # metadata = {
-        #   uuid = "f7542282-97a2-4bf5-ade9-c4a34e3ff7d1"
-        # }
-        # id = "28ccb338-f572-4088-9bb7-fd7e4ede8282"
-      }
+      # resource "nutanix_image" "image" {
+      #   # name        = "ubuntu-20.04.2.0-desktop-amd64.iso"
+      #   name        = "ubuntu-20.04.5-desktop-amd64.iso"
+      #   # description = "Ubuntu 20.04 LTS"
+      #   source_uri = "https://releases.ubuntu.com/focal/ubuntu-20.04.5-desktop-amd64.iso"
+      #   # id = "f7542282-97a2-4bf5-ade9-c4a34e3ff7d1"
+      #   # metadata = {
+      #   #   uuid = "f7542282-97a2-4bf5-ade9-c4a34e3ff7d1"
+      #   # }
+      #   # id = "28ccb338-f572-4088-9bb7-fd7e4ede8282"
+      # }
 
     #   resource "nutanix_virtual_machine" "vm" {
     #     name                 = "MyVM from the Terraform Nutanix Provider"
