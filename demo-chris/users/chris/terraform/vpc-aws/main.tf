2ac7b0841b5c2081fba9c96ca48c653c3cba1c66
mikeNetHopper <98332038+mikeNetHopper@users.noreply.github.com>
2023-01-11 09:20:13 -0500

    Update main.tf

--- demo-chris/users/chris/terraform/vpc-aws/main.tf
+++ demo-chris/users/chris/terraform/vpc-aws/main.tf
@@ -1,16 +1,16 @@
-# provider "aws" {
-#   shared_credentials_file = "aws-creds.ini"
-#   region = "us-east-2"
-# }
+provider "aws" {
+  shared_credentials_file = "aws-creds.ini"
+  region = "us-east-2"
+}
 
-# // Modules _must_ use remote state. The provider does not persist state.
-# terraform {
-#   backend "kubernetes" {
-#     secret_suffix     = "providerconfig-default"
-#     namespace         = "nethopper"
-#     in_cluster_config = true
-#   }
-# }
+// Modules _must_ use remote state. The provider does not persist state.
+terraform {
+  backend "kubernetes" {
+    secret_suffix     = "providerconfig-default"
+    namespace         = "nethopper"
+    in_cluster_config = true
+  }
+}
 resource "aws_vpc" "main" {
  cidr_block = "10.0.0.0/16"
  
