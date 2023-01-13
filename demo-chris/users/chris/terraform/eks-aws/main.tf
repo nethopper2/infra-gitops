5a400092e5339c0185f79cd7efedda3e7bdf463e
Chris Munford <82123419+cmunford73@users.noreply.github.com>
2023-01-13 16:01:39 -0500

    Update main.tf

--- demo-chris/users/chris/terraform/eks-aws/main.tf
+++ demo-chris/users/chris/terraform/eks-aws/main.tf
@@ -4,7 +4,7 @@
 # The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
 # You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
 provider "aws" {
-  shared_credentials_file = "aws-creds.ini"
+  shared_credentials_files = "aws-creds.ini"
   region = "us-west-1"
 }
 
