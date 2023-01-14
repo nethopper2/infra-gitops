df91492eb8a7d288a91bb527980b3fd600fa136f
Dan Donahue <ddonahuex@gmail.com>
2023-01-14 13:11:04 -0500

    Add VCP module and workspace to tf demo

---
+++ aws-xplane-tf-demo/modules/vpc-aws/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  shared_credentials_file = "aws-creds.ini"
+  region = "us-east-2"
+}
+
+// Modules _must_ use remote state. The provider does not persist state.
+terraform {
+  backend "kubernetes" {
+    secret_suffix     = "providerconfig-default"
+    namespace         = "devops-admin-tf"
+    in_cluster_config = true
+  }
+}
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+ 
+ tags = {
+   Name = "aws-xplane-tf-demo VPC"
+ }
+}
