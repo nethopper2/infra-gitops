3e52486a198ada41dc5057503299ecc40cc4ae2e
cmunford73 <chris@nethopper.io>
2023-03-19 17:00:07 -0400

    putting azurerm config in main.tf, except for secret

--- demo/users/chris/terraform/vpc-azure/main.tf
+++ demo/users/chris/terraform/vpc-azure/main.tf
@@ -12,6 +12,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+  client_id = "40872f6d-74b1-4f0c-a560-7d754d8fd3dd"
+  tenant_id = "6cea5d02-bfe1-4887-89f4-1dae00a54c60"
+  subscription_id = "b471e439-04ca-42fe-af9c-a2eedb46cdfa"
 }
 
 resource "azurerm_resource_group" "rg" {
