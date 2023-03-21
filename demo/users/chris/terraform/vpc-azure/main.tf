7d3437e1ba91f633b9738be3b32cafe2cf18bce6
Chris Munford <82123419+cmunford73@users.noreply.github.com>
2023-03-21 07:18:43 -0400

    Update main.tf

--- demo/users/chris/terraform/vpc-azure/main.tf
+++ demo/users/chris/terraform/vpc-azure/main.tf
@@ -17,13 +17,13 @@ terraform {
 }
 
 # // Modules _must_ use remote state. The provider does not persist state.
-# # terraform {
-# #   backend "kubernetes" {
-# #     secret_suffix     = "providerconfig-default"
-# #     namespace         = "default"
-# #     in_cluster_config = true
-# #   }
-# # }
+terraform {
+  backend "kubernetes" {
+    secret_suffix     = "providerconfig-default"
+    namespace         = "default"
+    in_cluster_config = true
+  }
+}
 
 provider "azurerm" {
   features {}
