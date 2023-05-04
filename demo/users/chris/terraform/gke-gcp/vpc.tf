d2da6e34d7cdd1b9525f620c97a41893b3db44cd
Chris Munford <82123419+cmunford73@users.noreply.github.com>
2023-05-04 09:32:33 -0400

    Update vpc.tf

--- demo/users/chris/terraform/gke-gcp/vpc.tf
+++ demo/users/chris/terraform/gke-gcp/vpc.tf
@@ -1,13 +1,6 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-variable "project_id" {
-  description = "project id"
-}
-
-variable "region" {
-  description = "region"
-}
 
 provider "google" {
   project = var.project_id
@@ -26,14 +19,14 @@ terraform {
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
+  name                    = "chris-vpc"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
-  region        = var.region
+  name          = "chris-subnet"
+  region        = us-central1
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 }
