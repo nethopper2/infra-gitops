e063419540ce0e15a6a836e031f1a89ff116866c
cmunford73 <chris@nethopper.io>
2023-05-04 12:46:49 -0400

    2nd var

--- demo/users/chris/terraform/gke-gcp/vpc.tf
+++ demo/users/chris/terraform/gke-gcp/vpc.tf
@@ -26,14 +26,14 @@ terraform {
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "chris-vpc"
+  name                    = "${var.project_id}-vpc"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "chris-subnet"
-  region        = "us-central1"
+  name          = "${var.project_id}-subnet"
+  region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 }
