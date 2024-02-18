0b8d99d9620620e31bdbf2d671c51aba2de70e3d
cmun <cmun@temp.org>
2024-02-18 13:09:18 -0500

    changed blue nodegroup to use t3.small

--- demo/users/chris/terraform/eks-aws-alb/modules/eks-cluster/main.tf
+++ demo/users/chris/terraform/eks-aws-alb/modules/eks-cluster/main.tf
@@ -48,9 +48,12 @@ module "eks" {
 
   eks_managed_node_groups = {
     blue = {
-	min_size     = 1
-	max_size     = 1
-	desired_size = 1
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+      instance_types = ["t3.nano"]
+      capacity_type  = "SPOT"
+
 	}
     green = {
       min_size     = 1
